b45c09e4aa7217d7452dcab1f202ecd64662c477
Arun CM <54279141+Arun4cm@users.noreply.github.com>
2022-02-05 00:47:31 +0530

    committing the basic programs

---
+++ Basic 10 programs/src/ninth.java
@@ -0,0 +1,11 @@
+public class ninth {
+	    public static void main(String[] args) {
+
+	        int num = 9, i = 1;
+	        while(i <= 10)
+	        {
+	            System.out.printf("%d * %d = %d \n", num, i, num * i);
+	            i++;
+	        }
+	    }
+	}
---
+++ Basic 10 programs/src/secound_one.java
@@ -0,0 +1,9 @@
+public class secound_one {
+	public static void main(String[] args) {
+        
+	      int num1 = 4747, num2 = 4747, sum;
+	      sum = num1 + num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	   }
+}
