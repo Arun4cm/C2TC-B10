7ec757ba85d12068a0b06007296a6156ecd54fbd
Arun CM <54279141+Arun4cm@users.noreply.github.com>
2022-02-10 18:14:54 +0530

    demo1

---
+++ demo on bufferreaderclass/src/demo1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class demo1 {
+	 public static void main(String[] args) throws IOException {
+	        
+	        int sum;
+	        InputStreamReader isr = new InputStreamReader();
+	        BufferedReader br = new BufferedReader(new isr(System.in));
+	        System.out.println("Enter First Number: ");
+	        int a = Integer.parseInt(br.readLine());
+	        
+	        System.out.println("Enter Second Number: ");
+	        int b = Integer.parseInt(br.readLine());
+	        
+	        
+	        sum = a + b;
+	        System.out.println("Sum of these numbers: "+sum);
+	    }
+}
