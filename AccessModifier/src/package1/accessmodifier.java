de7a46b4281ba41ed6e3a7147c1d21052ca8dc44
Arun CM <54279141+Arun4cm@users.noreply.github.com>
2022-02-11 00:26:09 +0530

    h

--- AccessModifier/src/package1/accessmodifier.java
+++ AccessModifier/src/package1/accessmodifier.java
@@ -1,11 +1,19 @@
 
 package package1;
-public class accessmodifier {
+import package2.accessmodifier3;
+public class accessmodifier extends accessmodifier3{
+	private int id = 4;
 	public static void main(String args[]) {
-		accessmodifier2 a = new accessmodifier2();
-		
+		accessmodifier2 a = new accessmodifier2();//default
+		accessmodifier3 b = new accessmodifier3();//public
+		accessmodifier c = new accessmodifier();//private
+		accessmodifier v = new accessmodifier();//protected
+
 		System.out.println(a.name);
 		System.out.println(a.city);	
-		
+		System.out.println(b.name);
+		System.out.println(v.city);	
+		System.out.println(c.id);	
+
 	}
 }
