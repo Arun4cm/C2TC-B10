68d9054ba3d2bb9c657f7ce1a134b96136ef7d3a
Arun CM <54279141+Arun4cm@users.noreply.github.com>
2022-02-20 16:37:49 +0530

    Declare and Initialize of an array

---
+++ arraydemo/src/arraydemo/arraydemo.java
@@ -0,0 +1,18 @@
+package arraydemo;
+
+public class arraydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[5];
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		for (int i=0; i<a.length; i++) {
+			System.out.println("Array value:" +a[i]);
+		}
+	}
+
+}
